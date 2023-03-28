3743891a0e2898738c176152c2e72202f3548de4
Ahmad Alkadri <ahmad@lekolabs.com>
2023-03-28 11:08:18 +0200

    Modify LICENSE and add try-catch error on the code

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+import sys
 
+if sys.version_info < (2, 6) or (3, 0) <= sys.version_info < (3, 6):
+    sys.exit("Sorry, Python version 2.6 and 3.0-3.5 are not supported.")
 
 setup(
     name             = 'cssmin',
     version          = '0.2.0',
-    author           = "Zachary Voase",
-    author_email     = "zacharyvoase@me.com",
-    url              = 'http://github.com/zacharyvoase/cssmin',
+    author           = "LEKO LABS",
+    url              = 'https://github.com/LekoLabs/cssmin',
     description      = "A Python port of the YUI CSS compression algorithm.",
     py_modules       = ['cssmin'],
     package_dir      = {'': 'src'},
